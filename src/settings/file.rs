53285dd4d0c54abb1f0ff8f29abb79119a73b05b
Dan Dowd <danieldowd89@gmail.com>
2022-11-13 12:43:33 -0700

    added global settings

--- src/settings/file.rs
+++ src/settings/file.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-/// Overwrites the entire content of a file with the provided string 
+/// Overwrites the entire content of a file with the provided string
 ///
 /// # Panics
 /// If there is an error while getting the file or writing to the file
@@ -10,34 +10,38 @@ pub fn overwrite_file(file_loc: &String, content: &String) {
     let mut file = get_file(file_loc);
     match file.write_all(content.as_bytes()) {
         Ok(file) => file,
-        Err(error) => panic!("Unable to overwrite file due to {:?}", error)
-    }; 
+        Err(error) => panic!("Unable to overwrite file due to {:?}", error),
+    };
 }
 
 /// Reads entire contents of a file into a string
 ///
 /// # Panics
-/// If there is an error while getting the file or reading the file 
+/// If there is an error while getting the file or reading the file
 /// Panics if .
 pub fn get_file_str(file_loc: &String) -> String {
     let mut file = get_file(file_loc);
     let mut content = String::new();
     match file.read_to_string(&mut content) {
         Ok(content_str) => content_str,
-        Err(error) => panic!("Unable to read content of file due to {:?}", error)
+        Err(error) => panic!("Unable to read content of file due to {:?}", error),
     };
     content
 }
 
-/// Retrieves a readable and writable file. It will create the file if it does not exist 
+/// Retrieves a readable and writable file. It will create the file if it does not exist
 ///
 /// # Panics
 /// If there is a problem while opening the file
 /// Panics if .
 fn get_file(file_loc: &String) -> File {
-    let file_result = File::options().create(true).write(true).read(true).open(file_loc);
+    let file_result = File::options()
+        .create(true)
+        .write(true)
+        .read(true)
+        .open(file_loc);
     match file_result {
         Ok(file) => file,
-        Err(error) => panic!("Error occurred while tyring to overwrite file: {:?}", error)
+        Err(error) => panic!("Error occurred while tyring to overwrite file: {:?}", error),
     }
 }
--- src/settings/global_settings.rs
+++ src/settings/global_settings.rs
@@ -1,12 +1,53 @@
 use std::collections::HashMap;
-use serde::{Serialize, Deserialize};
-use serde_json::Value;
+use serde::{Serialize, Deserialize,de};
+use serde_json::{Value, from_str, from_value};
 use dirs;
+
+use super::file;
 
 #[derive(Serialize, Deserialize)]
 pub struct GlobalSettings {
-    #[serde(default)]
+    #[serde(default = "get_config_dir")]
     pub working_dir: String, 
 
+    #[serde(default)]
     pub module_settings: HashMap<String, Value>
+}
+
+pub fn get_module_settings<T: de::DeserializeOwned>(module_name: &String) -> T {
+   let global_settings = get_global_settings();
+   let module_settings = match global_settings.module_settings.get(module_name) {
+        Some(module_settings) => module_settings,
+        None => panic!("Module {module_name} has not initialized it's settings", module_name = module_name)
+   };
+
+   let module_settings: T = match from_value(module_settings.to_owned()) {
+       Ok(settings) => settings,
+       Err(error) => panic!("Unable to parse module settings due to error {:?}", error)
+   };
+
+   module_settings
+} 
+
+pub fn get_global_settings() -> GlobalSettings {
+    let global_settings_dir = get_config_dir();
+    let global_settings_file = format!("{dir}/curld-settings.json", dir = global_settings_dir);
+    let global_settings_str = file::get_file_str(&global_settings_file);
+
+    match from_str(&global_settings_str) {
+        Ok(global_settings) => global_settings,
+        Err(error) => panic!("Unable to serialize settings due to error: {:?}", error)
+    }
+}
+
+fn get_config_dir() -> String {
+    let path_buf = match dirs::config_dir() {
+        Some(dir) => dir,
+        None => panic!("Unable to OS config dir")
+    };
+
+    match path_buf.to_str() {
+        Some(dir_str) => dir_str.to_owned(),
+        None => panic!("Unable to convert config dir to string")
+    }
 }

7801a5bb6c7b84dafd9033b28b4e1046320dd504
Dan Dowd <danieldowd89@gmail.com>
2022-11-13 11:10:08 -0700

    added test

--- src/settings/file.rs
+++ src/settings/file.rs
@@ -1,6 +1,11 @@
-use std::fs::{File};
+use std::fs::File;
 use std::io::prelude::*;
 
+/// Overwrites the entire content of a file with the provided string 
+///
+/// # Panics
+/// If there is an error while getting the file or writing to the file
+/// Panics if .
 pub fn overwrite_file(file_loc: &String, content: &String) {
     let mut file = get_file(file_loc);
     match file.write_all(content.as_bytes()) {
@@ -9,6 +14,11 @@ pub fn overwrite_file(file_loc: &String, content: &String) {
     }; 
 }
 
+/// Reads entire contents of a file into a string
+///
+/// # Panics
+/// If there is an error while getting the file or reading the file 
+/// Panics if .
 pub fn get_file_str(file_loc: &String) -> String {
     let mut file = get_file(file_loc);
     let mut content = String::new();
@@ -19,6 +29,11 @@ pub fn get_file_str(file_loc: &String) -> String {
     content
 }
 
+/// Retrieves a readable and writable file. It will create the file if it does not exist 
+///
+/// # Panics
+/// If there is a problem while opening the file
+/// Panics if .
 fn get_file(file_loc: &String) -> File {
     let file_result = File::options().create(true).write(true).read(true).open(file_loc);
     match file_result {

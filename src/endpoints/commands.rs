6aa3f3be8544caa5f4439dbd485257733a489a88
Dan Dowd <danieldowd89@gmail.com>
2022-11-13 15:58:56 -0700

    added tests

--- src/endpoints/commands.rs
+++ src/endpoints/commands.rs
@@ -1,3 +1,18 @@
+use std::collections::HashSet;
+
+pub struct Templates {
+    endpoint: Option<Vec<String>>,
+    method: Option<String>,
+    data: Option<Vec<String>>,
+    base_url: Option<Vec<String>>,
+    headers: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+pub struct InputError {
+    pub details: String
+}
+
 pub fn construct_curl_endpoint(
     endpoint: String,
     method: String,
@@ -21,14 +36,11 @@ pub fn construct_curl_endpoint(
                 header_str.push_str(&format!("--header {}", header))
             }
             header_str
-        },
-        None => "".to_string()
-    };
-
-    let data_str = match data {
-        Some(data) => data,
+        }
         None => "".to_string(),
     };
+
+    let data_str = data.unwrap_or("".to_string());
 
     format!(
         "--request {method}{headers}{data} {url}",
@@ -37,4 +49,46 @@ pub fn construct_curl_endpoint(
         data = data_str,
         url = url
     )
+}
+
+fn extract_template_names(templated: &String) -> Result<Vec<String>, InputError> {
+    // Use a HashSet to ensure there are no duplicates
+    let mut names: HashSet<String> = HashSet::new();
+    let mut alt_templated = templated.clone();
+    loop {
+        let start_index = match alt_templated.find("{") {
+            Some(index) => index,
+            None => break
+        };
+
+        let end_index = match alt_templated.find("}"){
+            Some(index) => index,
+            None => return Err(InputError {
+                details: String::from("Parsing error in template: found opening brace but no closing")
+            })
+        };
+
+        let template_name = String::from(&alt_templated[start_index + 1..end_index]);
+        names.insert(template_name);
+
+        alt_templated = String::from(&alt_templated[end_index + 1..]);
+    };
+
+    Ok(Vec::from_iter(names))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn extract_template_names_should_parse() {
+        let test_str = String::from("https://{base_url}/v1/{endpoint}");
+
+        let mut names = extract_template_names(&test_str).unwrap();
+        // Order the vector because the HashSet order is non-deterministic
+        names.sort();
+        assert_eq!(names.get(0).unwrap(), "base_url");
+        assert_eq!(names.get(1).unwrap(), "endpoint")
+    }
 }

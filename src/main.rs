3244a3ddfefafdb1192f37dc025d411e697c6d07
Dan Dowd <danieldowd89@gmail.com>
2022-11-15 21:32:03 -0700

    fixed mut error

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use endpoints::endpoint_settings;
 use settings::global_settings;
 
 fn main() {
-    let global_settings = global_settings::init();
+    let mut global_settings = global_settings::init();
     global_settings.init_module(&String::from("endpoints"), endpoint_settings::default());
     global_settings.write();
 

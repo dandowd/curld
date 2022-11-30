c3bec59ab8e634378ef043f44e3476358d1192a6
Dan Dowd <danieldowd89@gmail.com>
2022-11-29 17:34:31 -0700

    task: refactor concerns

--- src/endpoints/endpoint_cli.rs
+++ src/endpoints/endpoint_cli.rs
@@ -1,8 +1,32 @@
+use std::collections::HashMap;
+
 use crate::endpoints::{
     endpoint_settings::EndpointSettings,
-    run_cli::{RunInput, run},
-    saved_cli::{SavedInput, saved}
+    run_cli::run,
+    saved_cli::{saved, SavedInput},
+    utils::{extract_template_names, insert_template_values, insert_template_values_vec},
 };
+
+#[derive(clap::Args, Debug)]
+pub struct RunInput {
+    #[arg(short)]
+    pub endpoint: String,
+
+    #[arg(short = 'X', long)]
+    pub method: String,
+
+    #[arg(short, long)]
+    pub base_url: Option<String>,
+
+    #[arg(short, long)]
+    pub data: Option<String>,
+
+    #[arg(short = 'H', long)]
+    pub headers: Option<Vec<String>>,
+
+    #[arg(short, long)]
+    pub id: Option<String>,
+}
 
 #[derive(clap::Subcommand, Debug)]
 pub enum Endpoints {
@@ -15,7 +39,34 @@ pub enum Endpoints {
 pub fn endpoints_match(endpoint_cmd: &Endpoints) {
     match endpoint_cmd {
         Endpoints::Run(input) => {
-            let curl_cmd = run(input); 
+            let RunInput {
+                endpoint,
+                base_url,
+                data,
+                headers,
+                method,
+                id,
+            } = input;
+            let header_str = headers.clone().unwrap_or(Vec::new());
+            let base_url_str = base_url.clone().unwrap_or("".to_string());
+            let data_str = data.clone().unwrap_or("".to_string());
+
+            let template_keys = get_template_keys(&endpoint, &data_str, &base_url_str, &header_str);
+            let user_templates = prompt_for_templates(template_keys); 
+
+            let endpoint = insert_template_values(&endpoint, &user_templates);
+            let data_str = insert_template_values(&data_str, &user_templates);
+            let base_url_str = insert_template_values(&base_url_str, &user_templates);
+            let header_str = insert_template_values_vec(&header_str, &user_templates);
+
+            let curl_cmd = run(
+                &endpoint,
+                &method,
+                &data_str,
+                &base_url_str,
+                &header_str,
+                &id,
+            );
             println!("{}", curl_cmd);
         }
         Endpoints::Saved(input) => {
@@ -31,4 +82,73 @@ pub fn endpoints_match(endpoint_cmd: &Endpoints) {
             }
         }
     }
+}
+
+pub struct Templates {
+    pub endpoint: Vec<String>,
+    pub data: Vec<String>,
+    pub base_url: Vec<String>,
+    pub headers: Vec<String>,
+}
+
+pub fn get_template_keys(
+    endpoint: &String,
+    data: &String,
+    base_url: &String,
+    headers: &Vec<String>,
+) -> Templates {
+    let endpoint_templates = extract_template_names(endpoint).unwrap();
+    let data_templates = extract_template_names(data).unwrap();
+    let base_url_templates = extract_template_names(base_url).unwrap();
+    let header_templates = {
+        let mut values = Vec::new();
+        for header in headers {
+            let mut template_names = extract_template_names(header).unwrap();
+            values.append(&mut template_names);
+        }
+
+        values
+    };
+
+    Templates {
+        endpoint: endpoint_templates,
+        data: data_templates,
+        base_url: base_url_templates,
+        headers: header_templates,
+    }
+}
+
+fn prompt_for_templates(template_keys: Templates) -> HashMap<String, String> {
+    let Templates {
+        endpoint,
+        data,
+        base_url,
+        headers,
+    } = template_keys;
+    let mut template_map: HashMap<String, String> = HashMap::new();
+    loop_prompt(endpoint, &mut template_map);
+    loop_prompt(data, &mut template_map);
+    loop_prompt(base_url, &mut template_map);
+    loop_prompt(headers, &mut template_map);
+
+    template_map
+}
+
+fn loop_prompt(template: Vec<String>, map: &mut HashMap<String, String>) {
+    for key in template {
+        if map.contains_key(&key) {
+            continue;
+        }
+        let mut value = String::new();
+        prompt_for_key(&key, &mut value);
+        map.insert(key, value);
+    }
+}
+
+fn prompt_for_key(key: &String, output: &mut String) {
+    use std::io::{stdin, stdout, Write};
+    print!("Enter value for {}: ", key);
+    let _ = stdout().flush();
+
+    stdin().read_line(output).expect("No input");
 }
--- src/endpoints/run_cli.rs
+++ src/endpoints/run_cli.rs
@@ -1,38 +1,16 @@
 use crate::endpoints::{
+    endpoint_settings::{EndpointSettings, SavedEndpoint},
     utils::construct_curl_cmd,
-    endpoint_settings::{ SavedEndpoint, EndpointSettings }
 };
 
-#[derive(clap::Args, Debug)]
-pub struct RunInput {
-    #[arg(short)]
-    endpoint: String,
-
-    #[arg(short = 'X', long)]
-    method: String,
-
-    #[arg(short, long)]
-    base_url: Option<String>,
-
-    #[arg(short, long)]
-    data: Option<String>,
-
-    #[arg(short = 'H', long)]
-    headers: Option<Vec<String>>,
-
-    #[arg(short, long)]
-    id: Option<String>,
-}
-
-pub fn run(input: &RunInput) -> String {
-    let RunInput {
-        endpoint,
-        method,
-        data,
-        base_url,
-        headers,
-        id,
-    } = input;
+pub fn run(
+    endpoint: &String,
+    method: &String,
+    data: &String,
+    base_url: &String,
+    headers: &Vec<String>,
+    id: &Option<String>,
+) -> String {
     let curl_cmd = construct_curl_cmd(endpoint, method, data, base_url, headers);
     if let Some(id_str) = id {
         let mut global_settings = crate::global_settings::get();

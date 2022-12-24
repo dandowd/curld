aaa3d0658c9df5bab44e5cddd6f40dd400a93d77
Dan Dowd <danieldowd89@gmail.com>
2022-12-24 11:17:10 -0600

    fix tests

--- src/endpoints/tests.rs
+++ src/endpoints/tests.rs
@@ -4,7 +4,7 @@ use crate::endpoints::utils::{extract_template_names, insert_template_values};
 
 #[test]
 fn extract_template_names_should_parse() {
-    let test_str = String::from("https://${base_url}/v1/${endpoint}");
+    let test_str = "https://${base_url}/v1/${endpoint}";
 
     let mut names = extract_template_names(&test_str).unwrap();
     // Order the vector because the HashSet order is non-deterministic
@@ -15,7 +15,7 @@ fn extract_template_names_should_parse() {
 
 #[test]
 fn extract_template_names_should_error_on_bad_parse() {
-    let test_str = String::from("https://${base_url/v1/${endpoint}");
+    let test_str = "https://${base_url/v1/${endpoint}";
 
     match extract_template_names(&test_str) {
         Ok(_) => panic!("extract template names succeeded when it should fail"),
@@ -25,7 +25,7 @@ fn extract_template_names_should_error_on_bad_parse() {
 
 #[test]
 fn insert_template_values_succeeds() {
-    let test_str = String::from("https://${base_url}/v1/${resource}/${resouceId}");
+    let test_str = "https://${base_url}/v1/${resource}/${resouceId}";
     let mut value_map: HashMap<String, String> = HashMap::new();
     value_map.insert("base_url".to_string(), "something.com".to_string());
     value_map.insert("resource".to_string(), "user".to_string());

45fc34b258361566aaa128f58cfd100f6f9af657
Dan Dowd <danieldowd89@gmail.com>
2022-12-25 17:35:45 -0600

    moved tests to file

--- src/endpoints/utils.rs
+++ src/endpoints/utils.rs
@@ -62,7 +62,7 @@ pub fn construct_curl_args(
 
     if !data.is_empty() {
         args.push("-d".to_string());
-        args.push( data.to_string());
+        args.push(data.to_string());
     };
 
     // Url should always be last
@@ -123,10 +123,7 @@ pub fn insert_template_values_vec(
     values
 }
 
-pub fn insert_template_values(
-    templated_str: &str,
-    value_map: &HashMap<String, String>,
-) -> String {
+pub fn insert_template_values(templated_str: &str, value_map: &HashMap<String, String>) -> String {
     let mut cloned_templated_str = templated_str.to_owned();
     for (key, value) in value_map {
         let replace_key = format!("${{{0}}}", key);
@@ -134,4 +131,44 @@ pub fn insert_template_values(
     }
 
     cloned_templated_str
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::endpoints::utils::{extract_template_names, insert_template_values};
+
+    #[test]
+    fn extract_template_names_should_parse() {
+        let test_str = "https://${base_url}/v1/${endpoint}";
+
+        let mut names = extract_template_names(&test_str).unwrap();
+        // Order the vector because the HashSet order is non-deterministic
+        names.sort();
+        assert_eq!(names.get(0).unwrap(), "base_url");
+        assert_eq!(names.get(1).unwrap(), "endpoint")
+    }
+
+    #[test]
+    fn extract_template_names_should_error_on_bad_parse() {
+        let test_str = "https://${base_url/v1/${endpoint}";
+
+        match extract_template_names(&test_str) {
+            Ok(_) => panic!("extract template names succeeded when it should fail"),
+            Err(err) => assert!(!err.is_empty()),
+        };
+    }
+
+    #[test]
+    fn insert_template_values_succeeds() {
+        let test_str = "https://${base_url}/v1/${resource}/${resouceId}";
+        let mut value_map: HashMap<String, String> = HashMap::new();
+        value_map.insert("base_url".to_string(), "something.com".to_string());
+        value_map.insert("resource".to_string(), "user".to_string());
+        value_map.insert("resouceId".to_string(), "uuid".to_string());
+
+        let replaced_str = insert_template_values(&test_str, &value_map);
+        assert_eq!(replaced_str, "https://something.com/v1/user/uuid")
+    }
 }

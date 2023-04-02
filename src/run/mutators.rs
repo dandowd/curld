33dbacb1c89913c3c18f40a30745d0d9e3570eb4
Dan Dowd <danieldowd89@gmail.com>
2023-04-02 13:59:20 -0600

    adds static string for opening and closing run

--- src/run/mutators.rs
+++ src/run/mutators.rs
@@ -4,14 +4,17 @@ use crate::variables::{parse, Extractor, Inserter};
 
 pub struct RunMutators {}
 
+static OPENING: &str = "{";
+static CLOSING: &str = "}";
+
 impl Inserter for RunMutators {
     fn insert(&self, templated: &str, value_map: &HashMap<String, String>) -> String {
-        parse::insert_variable_values(templated, value_map, "${", "}")
+        parse::insert_variable_values(templated, value_map, OPENING, CLOSING)
     }
 }
 
 impl Extractor for RunMutators {
     fn extract(&self, templated: &str) -> Vec<String> {
-        parse::extract_variable_names(templated, "${", "}")
+        parse::extract_variable_names(templated, OPENING, CLOSING)
     }
 }
